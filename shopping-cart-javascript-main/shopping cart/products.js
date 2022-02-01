0f8ae62dbb786520c37c8cc43600eb9c935f352f
eshandawadi <eshandawadi123@gmail.com>
2022-02-01 07:38:51 +0545

    first commit

---
+++ shopping-cart-javascript-main/shopping cart/products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Samsung Galaxy ",
+    price: 299,
+    instock: 100,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/mobile.png",
+  },
+  {
+    id: 1,
+    name: "Mouse",
+    price: 5,
+    instock: 43,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/mouse.png",
+  },
+  {
+    id: 2,
+    name: "Jacket",
+    price: 20,
+    instock: 10,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/clothes.png",
+  },
+  {
+    id: 3,
+    name: "Iphone",
+    price: 500,
+    instock: 5,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/iphone.png",
+  },
+  {
+    id: 4,
+    name: "Goldstar shoes",
+    price: 29.99,
+    instock: 4,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/shoes.jpg",
+  },
+  {
+    id: 5,
+    name: " Nike Shoes",
+    price: 39.99,
+    instock: 40,
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores, error.",
+    imgSrc: "./img/nike.jpg",
+  },
+];
